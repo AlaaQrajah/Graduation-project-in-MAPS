833941941321778fe29a059ea33a5282a4127c98
Alaa Kracah <alaaqaraja2004@gmail.com>
2025-03-10 21:51:24 +0300

    Logo formatting, search and teaming

--- src/components/GameHeading.jsx
+++ src/components/GameHeading.jsx
@@ -1,5 +1,7 @@
 export const GameHeading = ({ selectGenre, selectPlatform }) => {
-    const heading = `${selectGenre?.name || ""} ${selectPlatform?.name || ""} Games`;
+  const heading = `${selectGenre?.name || ""} ${
+    selectPlatform?.name || ""
+  } Games`;
 
-    return <h1 className="text-4xl font-extrabold">{heading}</h1>;
+  return <h1 className="text-4xl font-extrabold">{heading}</h1>;
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,45 +1,39 @@
-// استيراد المكونات والصور المطلوبة.
-import Logo from "./Logo"; // استيراد مكون الشعار
-import LogoImag from "../assets/images/logo.webp"; // استيراد صورة الشعار
-import SearchInput from "./SearchInput"; // استيراد مكون البحث
-import "../App.css"; // استيراد ملف الأنماط (CSS) الخاص بالتطبيق
+import Logo from "./Logo";
+import LogoImag from "../assets/images/logo.webp";
+import SearchInput from "./SearchInput";
+import "../App.css";
 
-// تعريف مكون NavBar الذي يستقبل 3 خصائص: toggleTheme (لتغيير الوضع)، theme (الوضع الحالي)، onSearch (دالة البحث).
 const NavBar = ({ toggleTheme, theme, onSearch }) => {
-
-  // دالة handleRefresh التي تقوم بإعادة تحميل الصفحة عند النقر على الشعار
   const handleRefresh = () => {
-    window.location.reload(); // إعادة تحميل الصفحة
+    window.location.reload();
   };
 
   return (
     <>
-      <div className="navbar-app"> {/* حاوية الشريط العلوي */}
-        
-        {/* قسم الشعار */}
+      <div className="navbar-app">
+        {" "}
         <div className="gamp-app-header">
-          <button onClick={handleRefresh} text="gamp app header"> {/* عند النقر على الزر، سيتم استدعاء دالة handleRefresh */}
-            <Logo image={LogoImag} /> {/* عرض الشعار باستخدام مكون Logo وتمرير صورة الشعار إليه */}
+          <button onClick={handleRefresh} text="gamp app header">
+            {" "}
+            <Logo image={LogoImag} />{" "}
           </button>
         </div>
-
-        {/* قسم حقل البحث */}
         <div className="cover-search">
-          <SearchInput onSearch={onSearch} /> {/* عرض مكون البحث وتمرير دالة onSearch إليه */}
+          <SearchInput onSearch={onSearch} />{" "}
         </div>
-
-        {/* قسم تبديل الوضعية (النهار/الليل) */}
         <div className="theme-mode">
-          <label className="switch"> {/* التسمية الخاصة بالتبديل بين الوضعين */}
+          <label className="switch">
+            {" "}
             <input
-              id="input" // تعيين الـ ID للـ input
-              type="checkbox" // تحديد نوع العنصر كـ checkbox
-              onChange={toggleTheme} // عند تغيير الوضعية، استدعاء الدالة toggleTheme
-              checked={theme === "dark"} // إذا كان الوضع الحالي هو "dark"، يتم تحديد الـ checkbox
+              id="input"
+              type="checkbox"
+              onChange={toggleTheme}
+              checked={theme === "dark"}
             />
-            <div className="slider round"> {/* الجزء الذي يحتوي على التبديل مع الشكل الدائري */}
-              <div className="sun-moon"> {/* الحاوية التي تحتوي على الأيقونات الخاصة بالشمس والقمر */}
-                {/* الأيقونات الخاصة بالقمر التي تظهر في الوضع الليلي */}
+            <div className="slider round">
+              {" "}
+              <div className="sun-moon">
+                {" "}
                 <svg id="moon-dot-1" className="moon-dot" viewBox="0 0 100 100">
                   <circle cx="50" cy="50" r="50"></circle>
                 </svg>
@@ -49,18 +43,27 @@ const NavBar = ({ toggleTheme, theme, onSearch }) => {
                 <svg id="moon-dot-3" className="moon-dot" viewBox="0 0 100 100">
                   <circle cx="50" cy="50" r="50"></circle>
                 </svg>
-                {/* الأيقونات الخاصة بالشمس التي تظهر في الوضع النهاري */}
-                <svg id="light-ray-1" className="light-ray" viewBox="0 0 100 100">
+                <svg
+                  id="light-ray-1"
+                  className="light-ray"
+                  viewBox="0 0 100 100"
+                >
                   <circle cx="50" cy="50" r="50"></circle>
                 </svg>
-                <svg id="light-ray-2" className="light-ray" viewBox="0 0 100 100">
+                <svg
+                  id="light-ray-2"
+                  className="light-ray"
+                  viewBox="0 0 100 100"
+                >
                   <circle cx="50" cy="50" r="50"></circle>
                 </svg>
-                <svg id="light-ray-3" className="light-ray" viewBox="0 0 100 100">
+                <svg
+                  id="light-ray-3"
+                  className="light-ray"
+                  viewBox="0 0 100 100"
+                >
                   <circle cx="50" cy="50" r="50"></circle>
                 </svg>
-
-                {/* الأيقونات الخاصة بالغيوم التي تظهر في الوضع الليلي والنهاري */}
                 <svg id="cloud-1" className="cloud-dark" viewBox="0 0 100 100">
                   <circle cx="50" cy="50" r="50"></circle>
                 </svg>
@@ -80,9 +83,7 @@ const NavBar = ({ toggleTheme, theme, onSearch }) => {
                   <circle cx="50" cy="50" r="50"></circle>
                 </svg>
               </div>
-              {/* النجوم التي تظهر في الوضع الليلي */}
               <div className="stars">
-                {/* النجوم التي تظهر في الوضع الليلي */}
                 <svg id="star-1" className="star" viewBox="0 0 20 20">
                   <path d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"></path>
                 </svg>
@@ -104,5 +105,4 @@ const NavBar = ({ toggleTheme, theme, onSearch }) => {
   );
 };
 
-// تصدير المكون NavBar لاستخدامه في أماكن أخرى من التطبيق
 export default NavBar;
