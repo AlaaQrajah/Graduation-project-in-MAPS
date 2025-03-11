7447d0cd3614203e2c5df4a8e5ae6e21c531e7dd
Alaa Kracah <alaaqaraja2004@gmail.com>
2025-03-11 03:02:41 +0300

    addd classes in scour

--- src/components/CriticScore.jsx
+++ src/components/CriticScore.jsx
@@ -1,10 +1,13 @@
+import "../App.css";
+
 const CriticScore = ({ score }) => {
   let backgroundColor =
     score > 75 ? "bg-orange-600" : score > 60 ? "bg-yellow-300" : "";
-  let textColor = score > 75 ? "text-white-600" : score > 60 ? "text-yellow-300" : "";
+  let textColor =
+    score > 75 ? "text-white-600" : score > 60 ? "text-yellow-300" : "";
   return (
     <span
-      className={`text-gray-50 ${backgroundColor} ${textColor} text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:${backgroundColor}  dark:${textColor}`}
+      className={`scour ${backgroundColor} ${textColor}  dark:${backgroundColor}  dark:${textColor}`}
     >
       {score}
     </span>
