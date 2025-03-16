a5b8c7c73ca2abbbf14c8a3e20c059ce2bbee417
Alaa Kracah <alaaqaraja2004@gmail.com>
2025-03-16 06:07:38 +0300

    Edit add css in GameHeading

--- src/components/Games/GameHeading.jsx
+++ src/components/Games/GameHeading.jsx
@@ -1,7 +1,8 @@
+import "../FilesCss/CssToFilesGame/CssForGameHeading.css";
 export const GameHeading = ({ selectGenre, selectPlatform }) => {
   const heading = `${selectGenre?.name || ""} ${
     selectPlatform?.name || ""
   } Games`;
 
-  return <h1 className="text-4xl font-extrabold">{heading}</h1>;
+  return <h1 className="Game-Heading-Title">{heading}</h1>;
 };
