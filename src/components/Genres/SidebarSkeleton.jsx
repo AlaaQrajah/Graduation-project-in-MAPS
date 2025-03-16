acf5c5f7fd2f3321cac7aa1353cb57b71d7f02ee
Alaa Kracah <alaaqaraja2004@gmail.com>
2025-03-16 03:03:10 +0300

    Edit add CSS file to SidebarSkeleton

--- src/components/Genres/SidebarSkeleton.jsx
+++ src/components/Genres/SidebarSkeleton.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import "../../App.css";
+import "../FilesCss/CssForSidebarSkeleton.css";
 
 const SidebarSkeleton = () => {
   return (
-    <div className="max-w-sm bg-gray-800 dark:bg-white border border-gray-200 dark:border-gray-700 rounded-lg shadow p-4 transition-colors">
-      <div className="p-5">
-        <div className="flex items-center gap-1">
-          <div className="w-14 h-9 bg-gray-300 dark:bg-gray-600 rounded animate-pulse"></div>
-          <div className="w-10 h-6 bg-gray-300 dark:bg-gray-600 rounded-md animate-pulse"></div>
+    <div className="skeleton-card">
+      <div className="skeleton-content-wrapper ">
+        <div className="skeleton-content  ">
+          <div className="skeleton-button "></div>
+          <div className="skeleton-badge "></div>
         </div>
       </div>
     </div>
