c2655abe93a7224404cc5ca36bfc84fe8a145e72
Alaa Kracah <alaaqaraja2004@gmail.com>
2025-03-16 04:56:58 +0300

    Edit add  css file in GameCard

--- src/components/Games/GameCard.jsx
+++ src/components/Games/GameCard.jsx
@@ -3,29 +3,31 @@ import CriticScore from "../CriticScore";
 import { Emoji } from "../Emoji";
 import PlatformIconList from "../Platform/PlatformIconList";
 import "../../App.css";
+import "../FilesCss/CssToFilesGame/CssForGameCard.css";
+
 const GameCard = ({ game }) => {
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-xl transform transition-all duration-300 hover:scale-105 hover:shadow-2xl dark:bg-gray-800 dark:border-gray-700">
-      <a href="#" className="block">
+    <div className="game-card">
+      <a href="#" className="game-link">
         <img
-          className="rounded-t-lg w-full h-60 object-cover"
+          className="game-image"
           src={getCroppedImageUrl(game.background_image)}
           alt={game.name}
         />
       </a>
-      <div className="p-5">
-        <div className="flex justify-between items-center mb-3">
-          <div className="platform">
+      <div className="game-info">
+        <div className="game-details">
+          <div className="game-platforms">
             <PlatformIconList
               platforms={game.parent_platforms.map((p) => p.platform)}
             />
           </div>
-          <div className="score">
+          <div className="game-score">
             <CriticScore score={game.metacritic} />
           </div>
         </div>
-        <a href="#" className="block">
-          <h5 className="mb-2 text-xl font-semibold text-gray-900 dark:text-white">
+        <a href="#" className="game-title-link">
+          <h5 className="game-title">
             {game.name} <Emoji rating={game.rating_top} />
           </h5>
         </a>
