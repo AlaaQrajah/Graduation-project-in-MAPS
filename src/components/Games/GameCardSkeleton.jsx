33eede7e938ef0b92664899e79933f5fa145e992
Alaa Kracah <alaaqaraja2004@gmail.com>
2025-03-16 05:58:07 +0300

    Edit add css in GameCardSkeleton

--- src/components/Games/GameCardSkeleton.jsx
+++ src/components/Games/GameCardSkeleton.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import "../../App.css";
+import "../FilesCss/CssToFilesGame/CssForGameCardSkeleton.css";
 const GameCardSkeleton = () => {
   return (
-    <div className=" game-card-skeleton max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <div className=" game-card-image rounded-t-lg bg-gray-300 w-full h-60 animate-pulse"></div>
-      <div className="game-card-content p-5">
-        <div className="game-card-title h-6 bg-gray-300 rounded-md mb-4 w-3/4 animate-pulse"></div>
-        <div className="game-card-icons flex justify-between items-center">
-          <div className="flex space-x-2">
-            <div className="icon-placeholder w-6 h-6 bg-gray-300 rounded-full animate-pulse"></div>
-            <div className="icon-placeholder w-6 h-6 bg-gray-300 rounded-full animate-pulse"></div>
-            <div className="icon-placeholder w-6 h-6 bg-gray-300 rounded-full animate-pulse"></div>
+    <div className="game-card-skeleton">
+      <div className="game-card-image"></div>
+      <div className="game-card-content">
+        <div className="game-card-title"></div>
+        <div className="game-card-icons">
+          <div className="game-content-placeholder">
+            <div className="icon-placeholder"></div>
+            <div className="icon-placeholder"></div>
+            <div className="icon-placeholder"></div>
           </div>
-          <div className="button-placeholder w-10 h-6 bg-gray-300 rounded-md animate-pulse"></div>
+          <div className="button-placeholder"></div>
         </div>
       </div>
     </div>
